fea6739e64817aeab772f7ba75e51fb7eb3e2cb6
Le Hoang Nam <lehoangnam040@gmail.com>
2024-04-08 20:27:43 +0700

    code

--- python/cengal/smem_receiver.py
+++ python/cengal/smem_receiver.py
@@ -1,10 +1,10 @@
-from cengal.hardware.memory.shared_memory import SharedMemory, wait_my_turn
+from cengal.hardware.memory.shared_memory import SharedMemory, wait_my_turn, numpy_array_memory_size
 import numpy as np
 import time
 
 shared_memory_name: str = 'namlh'
 shape = (100, 100, 3)
-shared_memory_size = int(np.prod(shape))
+shared_memory_size = max(1024*10, numpy_array_memory_size(shape, np.uint8) * 2) 
 consumer: SharedMemory = SharedMemory(shared_memory_name, False, shared_memory_size)
 try:
     import _posixshmem
@@ -14,23 +14,26 @@ try:
 except FileNotFoundError:
     pass
 
-img = np.random.rand(*shape).astype(np.uint8)
 
 def main():
     print('Consumer is ready')
     need_to_stop = False
     while not need_to_stop:
-        start = time.perf_counter()
+        
         with wait_my_turn(consumer, periodic_sleep_time=None):
+            start = time.perf_counter()
             while consumer.has_messages():
-                new_message = consumer.take_message()
-                if new_message is None:
+                obj, obj_offset = consumer.take_message_2()
+                if obj is None:
                     need_to_stop = True
+                else:
+                    _ = np.frombuffer(obj, dtype=np.uint8).reshape(shape)
+                if obj_offset:
+                    consumer.destroy_object(obj_offset)
+                if need_to_stop:
                     break
-                print(new_message)
-                _ = np.frombuffer(new_message, dtype=np.uint8).reshape(shape)
-        stop = time.perf_counter()
-        print("Read Duration:", (stop - start) * 1000, "ms")
+            stop = time.perf_counter()
+            print("Read Duration:", (stop - start) * 1000, "ms")
 
 if '__main__' == __name__:
     main()
--- python/cengal/smem_sender.py
+++ python/cengal/smem_sender.py
@@ -1,4 +1,4 @@
-from cengal.hardware.memory.shared_memory import SharedMemory, wait_my_turn
+from cengal.hardware.memory.shared_memory import SharedMemory, wait_my_turn, FreeMemoryChunkNotFoundError, numpy_array_memory_size
 import numpy as np
 import time
 
@@ -16,11 +16,10 @@ except FileNotFoundError:
     pass
 
 shape = (100, 100, 3)
-shared_memory_size = int(np.prod(shape))
-print(shared_memory_size)
+shared_memory_size = max(1024*10, numpy_array_memory_size(shape, np.uint8) * 2) 
 creator: SharedMemory = SharedMemory(shared_memory_name, True, shared_memory_size)
 
-img = np.random.rand(*shape).astype(np.uint8)
+img = np.random.randint(0, 255, size=shape, dtype=np.uint8)
 
 def main():
     print('Sender is waiting for consumers')
@@ -28,15 +27,20 @@ def main():
     print('Sender is ready')
     try:
         while True:
-            start = time.perf_counter()
-            with wait_my_turn(creator, periodic_sleep_time=None):
-                x = img.tobytes()
-                print(type(x), len(x))
-                creator.put_message(x)
-            stop = time.perf_counter()
-            print("Writing Duration:", (stop - start) * 1000, "ms")
+            try:            
+                with wait_my_turn(creator, periodic_sleep_time=None):
+                    start = time.perf_counter()
+                    x = img.tobytes()
+                    creator.put_message(x)
+                    stop = time.perf_counter()
+                delta_time = stop - start
+                print("Writing Duration:", delta_time * 1000, "ms")
+                print("Speed:", 1 / delta_time, "puts/s")
+            except FreeMemoryChunkNotFoundError as ex:
+                raise
+                # print("got FreeMemoryChunkNotFoundError", ex)
     except KeyboardInterrupt:
-        pass
+        print("Keyboard interrupt")
     finally:
         with wait_my_turn(creator, periodic_sleep_time=None):
             creator.put_message(None)
